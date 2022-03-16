44a4d66d5fc428037d83f57eb9e0f0e72df7cdca
KacpiCzeci <k.trzeciak97@gmail.com>
2022-03-16 03:05:41 +0100

    Add own Generic Collection

--- 02-LINQ/TSD.Linq.Task2.Lib/LeapYear.cs
+++ 02-LINQ/TSD.Linq.Task2.Lib/LeapYear.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 
 namespace TSD.Linq.Task2.Lib {
 
@@ -9,6 +10,26 @@ namespace TSD.Linq.Task2.Lib {
     	    int year = 2000;
             LeapYear calculator = new LeapYear();
             Console.WriteLine(calculator.calculateLeapYear(year));
+        }
+    }
+
+    public class randomCollection: Collection<int> {
+        private Random rnd;
+
+        public randomCollection(){
+            this.rnd = new Random();
+        }
+
+        public void Add(int item){
+            base.InsertItem(this.rnd.Next(0, base.Count()), item);
+        }
+
+        public int Get(int index){
+            return base.Item(this.rnd.Next(0, index <= base.Count()? index : base.Count()));
+        }
+
+        public bool isEmpty(){
+            return base.Count() == 0;
         }
     }
 }
