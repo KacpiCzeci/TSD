d7db427d66603f364dbbfba9e40b8a9cb057e471
KacpiCzeci <kapixxx666@wp.pl>
2022-04-23 03:28:48 +0200

    Task 1,2 from Intermediate

--- 04-WPF/MainWindow.xaml.cs
+++ 04-WPF/MainWindow.xaml.cs
@@ -21,14 +21,78 @@ namespace _04_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Book> bookList;
         public MainWindow()
         {
             InitializeComponent();
+<<<<<<< Updated upstream
             this.BookList.ItemsSource = MyBookCollection.GetMyCollection();
         }
 
         public void getBooks(){
             this.BookList.ItemsSource = MyBookCollection.GetMyCollection();
+=======
+            this.bookList = MyBookCollection.GetMyCollection();
+            this.BookList.ItemsSource = this.bookList;
+>>>>>>> Stashed changes
         }
+
+        public void getBooks(){
+            this.BookList.ItemsSource = MyBookCollection.GetMyCollection();
+        }
+
+        public void addComponent(object sender, RoutedEventArgs e){
+            if(this.BookList.SelectedIndex != -1){
+                this.BookList.UnselectAll();
+                this.Details.Visibility = Visibility.Hidden;
+                this.Add.Visibility = Visibility.Visible;
+            }
+            else if(this.Add.Visibility == Visibility.Visible) {
+                if(string.IsNullOrEmpty(this.ID.Text) || string.IsNullOrEmpty(this.Author.Text) || string.IsNullOrEmpty(this.Title.Text) || string.IsNullOrEmpty(this.Year.Text)){
+                    MessageBox.Show("Please provide all data!");
+                }
+                else{
+                    bookList.Add(new Book(int.Parse(this.ID.Text)){ 
+                        Author = this.Author.Text, 
+                        Format = BookFormat.EBook, 
+                        IsRead = false, 
+                        Title = this.Title.Text, 
+                        Year= int.Parse(this.Year.Text)
+                    });
+                    this.ID.Clear();
+                    this.Author.Clear();
+                    this.Format.Clear();
+                    this.IsRead.Clear(); 
+                    this.Title.Clear();
+                    this.Year.Clear();
+                    this.BookList.Items.Refresh();
+                }
+            }
+        }
+
+        public void showDetails(object sender, MouseButtonEventArgs e){
+            if (e.ClickCount == 1 && Keyboard.Modifiers == ModifierKeys.Control) {
+                this.Details.Visibility = Visibility.Hidden;
+                this.Add.Visibility = Visibility.Visible;
+            }
+            if (e.ClickCount == 1 && (Keyboard.Modifiers & ModifierKeys.Control) == 0) {
+                this.Add.Visibility = Visibility.Hidden;
+                this.Details.Visibility = Visibility.Visible;
+            }
+        }
+
+        public void outerButton(object sender, RoutedEventArgs e){
+            if(this.BookList.SelectedIndex != -1){
+                bookList.Remove((Book)this.BookList.SelectedItem);
+                this.BookList.Items.Refresh();
+            }
+            this.Details.Visibility = Visibility.Hidden;
+        } 
+
+        public void innerButton(object sender, RoutedEventArgs e){
+            if(this.BookList.SelectedIndex != -1){
+                MessageBox.Show("Book was deleted!");
+            }
+        } 
     }
 }
