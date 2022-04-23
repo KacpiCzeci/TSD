bd580e7ff18cab4b37bbdb0b573e12d626d06395
KacpiCzeci <kapixxx666@wp.pl>
2022-04-23 22:11:20 +0200

    Task 4 + app styling

--- 04-WPF/MainWindow.xaml.cs
+++ 04-WPF/MainWindow.xaml.cs
@@ -29,10 +29,6 @@ namespace _04_WPF
             this.BookList.ItemsSource = this.bookList;
         }
 
-        public void getBooks(){
-            this.BookList.ItemsSource = MyBookCollection.GetMyCollection();
-        }
-
         public void addComponent(object sender, RoutedEventArgs e){
             if(this.BookList.SelectedIndex != -1){
                 this.BookList.UnselectAll();
@@ -43,18 +39,21 @@ namespace _04_WPF
                 if(string.IsNullOrEmpty(this.ID.Text) || string.IsNullOrEmpty(this.Author.Text) || string.IsNullOrEmpty(this.Title.Text) || string.IsNullOrEmpty(this.Year.Text)){
                     MessageBox.Show("Please provide all data!");
                 }
+                else if(!int.TryParse(this.ID.Text, out _) || !int.TryParse(this.Year.Text, out _)){
+                    MessageBox.Show("Wrong data!");
+                }
                 else{
                     bookList.Add(new Book(int.Parse(this.ID.Text)){ 
                         Author = this.Author.Text, 
-                        Format = BookFormat.EBook, 
-                        IsRead = false, 
+                        Format = this.Format.SelectedItem == null? BookFormat.PaperBack: (BookFormat)this.Format.SelectedItem, 
+                        IsRead = this.IsRead.IsChecked ?? false, 
                         Title = this.Title.Text, 
                         Year= int.Parse(this.Year.Text)
                     });
                     this.ID.Clear();
                     this.Author.Clear();
-                    this.Format.Clear();
-                    this.IsRead.Clear(); 
+                    this.Format.SelectedItem = -1;
+                    this.IsRead.IsChecked = false; 
                     this.Title.Clear();
                     this.Year.Clear();
                     this.BookList.Items.Refresh();
@@ -91,5 +90,35 @@ namespace _04_WPF
             int value = (int)(this.Slider.Value) > 255? 255: (int)(this.Slider.Value) < 0? 0: (int)(this.Slider.Value);
             this.fgrid.Background = new SolidColorBrush(Color.FromArgb((byte)value, 0, (byte)value, 100));
         } 
+    }
+
+    public class ColorConverter : IValueConverter{
+        
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture){
+            BookFormat format = (BookFormat)value;
+            if(format == BookFormat.PaperBack){
+                return Brushes.Gainsboro;
+            }
+            return Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture){
+            throw new NotImplementedException();
+        }
+    }
+
+    public class StyleConverter : IValueConverter{
+        
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture){
+            bool isRead = (bool)value;
+            if(isRead == true){
+                return (Style)Application.Current.FindResource("ItemTextBlack");
+            }
+            return (Style)Application.Current.FindResource("ItemTextRed");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture){
+            throw new NotImplementedException();
+        }
     }
 }
